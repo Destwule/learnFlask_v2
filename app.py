0ef26c9f4194ef0968c57a7ccb64f202c0b02752
Destiny Wulelazi Godfrey <destinyw.godfrey@gmail.com>
2024-12-11 18:29:40 +0100

    dynamic pages and apply form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from db import load_jobs_from_db, Job, load_job_from_db
 
 
@@ -17,14 +17,18 @@ def list_jobs():
 	jobs = load_jobs_from_db()
 	return jsonify(jobs)
 
-
-
 @app.route('/job/<id>')
 def show_job(id):
 	job = load_job_from_db(id)
 	if not job:
 		return '<h1>Not found</h1>'
 	return render_template('jobpage.html', job=job)
+
+@app.route('/job/<id>/apply', methods=['post'])
+def apply_to_job(id):
+	data = request.form
+	job = load_job_from_db(id)
+	return render_template("application_submited.html", application=data, job=job)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
